765ed8e6b8c57e1118428737c6f78cc0ac60089b
mdsanima <marcinrozewski@gmail.com>
2021-08-12 16:49:59 +0200

    docs: add theme and html option closes #4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,6 +57,24 @@ source_suffix = {
 exclude_patterns = []
 
 
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'sphinx_book_theme'
+html_logo = "_static/icons/ic_mdsanima_08_lng_drp_sdw_c.png"
+html_favicon = "_static/icons/ic_mdsanima_09_smp_w.png"
+panels_add_bootstrap_css = False
+
+html_theme_options = {
+    "repository_url": "https://github.com/mdsanima-dev/mdsanima-dev",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": False,
+    "home_page_in_toc": True,
+    "show_navbar_depth": 2,
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
