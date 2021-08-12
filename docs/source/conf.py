38b3552116817fce65fc1e6b45587e43a9e5faea
mdsanima <marcinrozewski@gmail.com>
2021-08-12 16:47:11 +0200

    docs: add `extension` and `markdown` parser closes #3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,17 +10,18 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+#sys.path.append(os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'mdsanima-dev'
-copyright = '2021, mdsanima'
-author = 'mdsanima'
+copyright = '2021, MDSANIMA'
+author = 'Marcin Rozewski'
+master_doc = 'index'
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,23 +29,33 @@ author = 'mdsanima'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'myst_parser',
+    'sphinx_panels',
+    'sphinx_copybutton',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.autosectionlabel',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+# Markdown Parser
+# myst_commonmark_only = True
+# autosectionlabel_prefix_document = True
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
