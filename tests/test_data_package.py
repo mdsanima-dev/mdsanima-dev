f52a40d5fec9f76670e838df9bf994817d98a5b3
mdsanima <marcinrozewski@gmail.com>
2022-04-05 17:02:55 +0200

    test: variable the same on `setyp.py` file
    the previous commit was just a rename

--- tests/test_data_package.py
+++ tests/test_data_package.py
@@ -3,37 +3,37 @@
 # Copyritht © 2022 Marcin Różewski MDSANIMA
 
 
-"""Testing loading package data from ``package.json`` file and printing output.
+"""Testing loading data package from ``package.json`` file and printing output.
 
-:usage: ``./test_package_data.py``
+:usage: ``./test_data_package.py``
 """
 
 
 import json
 
 
-def test_package_data():
+def test_data_package():
     with open("../package.json") as dt:
-        package_data = json.load(dt)
+        data_package = json.load(dt)
 
-    name = package_data["name"]
-    version = package_data["version"]
-    author_name = package_data["author"]["name"]
-    author_email = package_data["author"]["email"]
-    description = package_data["description"]
-    homepage = package_data["homepage"]
-    project_urls = package_data["project_urls"]
-    classifiers = package_data["classifiers"]
-    license = package_data["license"]
-    extras_require = package_data["extra_require"]
-    keywords = package_data["keywords"]
+    name = data_package["name"]
+    version = data_package["version"]
+    author = data_package["author"]["name"]
+    author_email = data_package["author"]["email"]
+    description = data_package["description"]
+    url = data_package["homepage"]
+    project_urls = data_package["project_urls"]
+    classifiers = data_package["classifiers"]
+    license = data_package["license"]
+    extras_require = data_package["extra_require"]
+    keywords = data_package["keywords"]
 
     print(name)
     print(version)
-    print(author_name)
+    print(author)
     print(author_email)
     print(description)
-    print(homepage)
+    print(url)
     print(project_urls)
     print(classifiers)
     print(license)
@@ -42,7 +42,7 @@ def test_package_data():
 
 
 def main():
-    test_package_data()
+    test_data_package()
 
 
 if __name__ == "__main__":
